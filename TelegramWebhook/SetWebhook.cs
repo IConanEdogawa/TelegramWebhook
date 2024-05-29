f174924534a255566142bab745918ac7e2c7378e
Conan Edogawa <160710144+IConanEdogawa@users.noreply.github.com>
2024-05-29 22:47:35 +0500

    Update SetWebhook.cs

--- TelegramWebhook/SetWebhook.cs
+++ TelegramWebhook/SetWebhook.cs
@@ -2,7 +2,7 @@
 {
     public class SetWebhook
     {
-        private static readonly string botToken = "6740900266:AAGebjzhHWN0C9V5j3hoL9iinPJ7CrTeVSk";
+        private static readonly string botToken = "UstozGitHubO`chirgizvordiTokenimi";
         private static readonly string webhookUrl = "https://localhost:7288/api/webhook"; // Замените на ваш реальный URL
         // https://localhost:7288/swagger/index.html
         static async Task Main(string[] args)
